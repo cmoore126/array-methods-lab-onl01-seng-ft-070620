27bcbd619124d630ce0be64a98159c0219bc6fa0
Clayton Moore <cmoorefour@gmail.com>
2020-07-08 17:35:48 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,11 +11,11 @@ def using_reverse(array)
 end
 
 def using_first(array)
- array.first
+ array.first(array)
 end
 
 def using_last(array)
- array.last
+ array.last(array)
 end
 
 def using_size(array)
